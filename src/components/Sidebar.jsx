94a4b8b1c35ce4c69673a6db5407ac8b165ad594
viLam11 <ngoc.huynhcs2212257@hcmut.edu.com>
2024-12-23 13:12:02 +0700

    fix Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,20 +1,27 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAddressCard } from "@fortawesome/free-regular-svg-icons";
 import { faImages } from "@fortawesome/free-regular-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 export default function Sidebar({page}) {
+    const navigate = useNavigate();
+
     return( 
         <div className="bg-white md:w-48 sticky h-full min-h-screen text-sm">
             <div className="logo">
                 <img src="https://geekup.vn/Icons/gu-logo-general.png" alt="GeekUp-logo" className="p-4" />
             </div>
             <div className="menu space-y-2 sm:hidden md:block">
-                <div className={`${page === "album" ? "bg-blue-50 text-blue-700" : ""} pl-6 font-normal rounded-lg m-2 py-2`}>
+                <div className={`${page === "album" ? "bg-blue-50 text-blue-700" : ""} hover:bg-gray-100 pl-6 font-normal rounded-lg m-2 py-2 cursor-auto`}
+                    onClick={() => navigate("/album")}
+                >
                     <span>
                         <FontAwesomeIcon icon={faImages} />
                     </span>  Albums
                 </div>
-                <div className={`${page === "user" ? "bg-blue-50 text-blue-700" : ""} pl-6 font-normal rounded-lg m-2 py-2`}>
+                <div className={`${page === "user" ? "bg-blue-50 text-blue-700" : ""} hover:bg-gray-100 pl-6 font-normal rounded-lg m-2 py-2 cursor-auto`}
+                    onClick={() => navigate('/user') }
+                >
                     <span>
                     <FontAwesomeIcon icon={faAddressCard} />
                     </span>  Users
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,8 +1,8 @@
 export default function User() {
-    return(
-        <>
-            This is User page
-        
-        </>
+    return (
+        <div className="flex flex-row">
+            <Sidebar page="album" />
+            <div className="w-full bg-gray-100"></div>
+        </div>
     )
 }
